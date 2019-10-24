e6ed1ad77af0d8f2abcd9c149b43b988ff350448
Estelle Olawinski <45394713+Olawinski-e@users.noreply.github.com>
2019-10-24 16:44:33 +0200

    //
    redone array & loops

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
 import "./styles.css";
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function getOdds(arr) {
+  let odds = [];
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (i % 2 !== 0) {
+      odds.push(i);
+    }
+  }
+  return odds;
+}
+
+console.log(getOdds(arr));
+
+function getOdds2(arr) {
+  return arr.filter(num => num % 2 !== 0);
+}
+console.log(getOdds2(arr));
+
+const getOdds3 = arr => arr.filter(num => num % 2 !== 0);
+console.log(getOdds3(arr));
+
+var arrayNames = ["Pedro", "Jake", "Joan"];
+arrayNames.forEach(name => console.log(name));
+
+const raceResults = ["Helen", "John", "Peter", "Merry"];
+raceResults.forEach(function(elem, index) {
+  console.log(elem + " finished the race in " + (index + 1) + " position!");
+});
+
+raceResults.forEach(elem => console.log(`${elem} finished the race in `));
+
+function printStars(howMany) {
+  console.log("*".repeat(howMany));
+}
+
+[1, 2, 3, 4, 5].forEach(function(num) {
+  printStars(num);
+});
+
+[1, 2, 3, 4, 5].forEach(num => printStars(num));
+
+var phrase = "This is long enough for a string to count it words";
+
+var words = phrase.split(" ");
+
+console.log(words);
+console.log(words[0]);
+console.log(words.length);
+
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello You!</h1>
 <div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
